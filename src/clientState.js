ec26fb60ebf23deda2ebce44045950c80fd75080
SEUNGMIN KIM <smk950226@naver.com>
2019-04-13 16:04:36 +0900

    setting defaults

--- src/clientState.js
+++ src/clientState.js
@@ -1,3 +1,31 @@
-export const resolvers = {};
-export const defaults = {};
-export const typeDefs = {};
+export const resolvers = {
+    Query: {
+        notes: () => []
+    }
+};
+
+export const defaults = {
+    notes: []
+};
+
+export const typeDefs = [
+    `
+    schema {
+        query: Query
+        mutation: Mutation
+    }
+    type Query {
+        notes: [Note]!
+        note(id: Int!): Note
+    }
+    type Mutation {
+        createNote(title: String!, content: String!)
+        editNote(id: Int!, title: String!, content: String!)
+    }
+    type Note{
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+];
