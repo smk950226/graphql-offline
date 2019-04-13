bcc4f529c068ece92981ff77f7945470984840db
SEUNGMIN KIM <smk950226@naver.com>
2019-04-13 17:12:58 +0900

    create note

--- src/clientState.js
+++ src/clientState.js
@@ -1,4 +1,5 @@
 import { NOTE_FRAGMENT } from './fragments';
+import { GET_NOTES } from './queries';
 
 export const resolvers = {
     Query: {
@@ -7,6 +8,24 @@ export const resolvers = {
             const note = cache.readFragment({fragment: NOTE_FRAGMENT, id})
             return note;
         }
+    },
+    Mutation: {
+        createNote: (_, variables, { cache }) => {
+            const { notes } = cache.readQuery({query: GET_NOTES});
+            const { title, content } = variables;
+            const newNote = {
+                __typename: "Note",
+                title,
+                content,
+                id: notes.length
+            };
+            cache.writeData({
+                data: {
+                    notes: [newNote, ...notes]
+                }
+            });
+            return newNote;
+        }
     }
 };
 
@@ -14,7 +33,7 @@ export const defaults = {
     notes: [
         {
             __typename: "Note",
-            id: 1,
+            id: 0,
             title: "First",
             content: "Test"
         }
