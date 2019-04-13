2cd9f10312d96518cb1494afbfb1fd4c4576610d
SEUNGMIN KIM <smk950226@naver.com>
2019-04-13 15:45:22 +0900

    setup for apollo

--- src/apollo.js
+++ src/apollo.js
@@ -2,3 +2,20 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { withClientState } from 'apollo-link-state';
 import { ApolloLink } from 'apollo-link';
+import { resolvers, typeDefs, defaults } from './clientState';
+
+const cache = new InMemoryCache();
+
+const stateLink = withClientState({
+    cache,
+    typeDefs,
+    defaults,
+    resolvers
+})
+
+const client = new ApolloClient({
+    cache,
+    link: ApolloLink.from([stateLink])
+})
+
+export default client
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
 import App from './App';
+import "./globalStyles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<ApolloProvider client={client}>
+    <App />
+</ApolloProvider>, 
+document.getElementById('root'));
